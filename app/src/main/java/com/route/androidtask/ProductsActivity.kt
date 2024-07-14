9b160c9e1cc48282c425775d0927ae0b9d63b631
AhmedMamdouh74 <amamdouh823@gmail.com>
2024-07-14 16:03:36 +0300

    add icons for products screen

---
+++ app/src/main/java/com/route/androidtask/ProductsActivity.kt
@@ -0,0 +1,15 @@
+package com.route.androidtask
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ProductsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_products)
+
+    }
+}
