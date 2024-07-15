04dc3dbe3bb431c3d5723aaf0742b1d764982001
AhmedMamdouh74 <amamdouh823@gmail.com>
2024-07-15 04:28:11 +0300

    using catch() to handel exceptions instead of try-catch due to testing

--- data/src/main/java/com/route/data/datasource/ProductsDataSourceImpl.kt
+++ data/src/main/java/com/route/data/datasource/ProductsDataSourceImpl.kt
@@ -6,27 +6,27 @@ import com.route.data.model.convertTo
 import com.route.domain.common.ResultWrapper
 import com.route.domain.model.ProductsItem
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class ProductsDataSourceImpl @Inject constructor(private val wepServices: WepServices) :
     ProductsDataSourceContract {
     override suspend fun getProducts(): Flow<ResultWrapper<List<ProductsItem?>?>> = flow {
-        try {
-            emit(ResultWrapper.Loading)
-            val response = wepServices.getProducts()
-            if (response.isSuccessful) {
-                response.body()?.let { body ->
-                    val products = body.products?.map { it?.convertTo(ProductsItem::class.java) }
-                    emit(ResultWrapper.Success(products))
-                } ?: emit(ResultWrapper.Error(Exception("Empty response body")))
-            } else {
-                emit(ResultWrapper.Error(Exception("API call failed with status: ${response.code()}")))
-            }
-        } catch (e: Exception) {
-            emit(ResultWrapper.Error(e))
+
+        emit(ResultWrapper.Loading)
+        val response = wepServices.getProducts()
+        if (response.isSuccessful) {
+            response.body()?.let { body ->
+                val products = body.products?.map { it?.convertTo(ProductsItem::class.java) }
+                emit(ResultWrapper.Success(products))
+            } ?: emit(ResultWrapper.Error(Exception("Empty response body")))
+        } else {
+            emit(ResultWrapper.Error(Exception("API call failed with status: ${response.code()}")))
         }
-
-
+    }.catch {
+        emit(ResultWrapper.Error(Exception(it.message)))
     }
+
+
 }
